63919808bd2e0be8a7b95453047e350c4fcd4b11
marcelogattermann <marcelo_gattermann@hotmail.com>
2023-11-28 19:33:13 -0300

    .

--- marcelo/23-11/resolucaodesafio.php
+++ marcelo/23-11/resolucaodesafio.php
@@ -1,7 +1,7 @@
 <?php
-echo "Deu certo!<br>";
-$valor = $_GET["num1"];
 
+$valor = $_GET["num1"];
+echo"Tabuada do ". $valor ."<br>";
 for ($i = 0; $i <= 10; $i++) {
     // echo $valor * $i;
     // echo"<br>";
